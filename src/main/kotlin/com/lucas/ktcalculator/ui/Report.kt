e063cb2f44e6800736dfc817d843ab66d3b0c936
Lucas-Ayabe <lucas.ayabe@gmail.com>
2025-01-24 01:55:40 -0300

    refactor(Report): removed useless elvis operator

--- src/main/kotlin/com/lucas/ktcalculator/ui/Report.kt
+++ src/main/kotlin/com/lucas/ktcalculator/ui/Report.kt
@@ -1,17 +1,17 @@
 package com.lucas.ktcalculator.ui
 
 class Report(private val answers: Map<String, String>) {
+    fun field(named: String): Field {
+        return Field(answers[named] ?: "")
+    }
+
     inner class Field(private val value: String) {
         fun asString(): String {
-            return value ?: ""
+            return value
         }
 
         fun asInt(): Int {
             return value.toIntOrNull() ?: 0
         }
-    }
-
-    fun field(named: String): Field {
-        return Field(answers[named] ?: "")
     }
 }
